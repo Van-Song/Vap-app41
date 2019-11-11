900b4dda82afe6b87bd48011bae6e9fefe20c365
Van <aaa@dddd>
2019-11-11 22:22:33 +0800

    homework-1

---
+++ Vap app4/AdidasVC.swift
@@ -0,0 +1,33 @@
+//
+//  AdidasVC.swift
+//  Vap app4
+//
+//  Created by Friend on 2019/11/8.
+//  Copyright © 2019 Friend. All rights reserved.
+//
+
+import UIKit
+
+class AdidasVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Exit2Click(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
